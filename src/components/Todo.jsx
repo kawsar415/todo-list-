66e22bd9ec59da8ec44a6c2de4be7b56609511aa
kawsar415 <ahamedkawsar415@gmail.com>
2025-09-14 16:52:03 +0600

    todo list

---
+++ src/components/Todo.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from 'react'
+import todo_icon from '../assets/todo_icon.png'
+import Todoitems from './todoitems'
+
+const Todo = () => {
+
+const [todoList, setTodoList] = useState([]);
+
+
+const inputRef = useRef();
+
+const add = () => {
+  const inputText = inputRef.current.value.trim();
+
+  if (inputText === "") {
+    return null;
+  }
+
+  const newTodo = {
+    id: Date.now(),
+    text: inputText,
+    isComplete: false,
+  }
+   setTodoList((prev) => [...prev, newTodo]);
+   inputRef.current.value = "";
+}
+
+const deleteTodo = (id) => {
+  setTodoList((prvTodos)=>{
+    return prvTodos.filter((todo) => todo.id !== id)
+  })
+}
+
+const toggle = (id)=>{
+  setTodoList((prevTodos) => {
+    return prevTodos.map((todo) => {
+      if (todo.id === id) {
+        return {...todo, isComplete: !todo.isComplete}
+      }
+      return todo;
+    })
+  })
+}
+
+useEffect(() => {
+  localStorage.setItem("todos", JSON.stringify(todoList));
+},[todoList])
+
+
+
+
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+      
+      {/* Title */}
+
+      <div className ='flex items-center mt-7 gap-2'>
+      <img className='w-8' src = {todo_icon} alt="" />
+      <h1 className='text-3xl font-semibold'> To-Do List</h1>
+      </div>
+
+      {/* input box */}
+
+      <div className = 'flex items-center gap-2 mt-7 bg-gray-200 rounded-full'>
+        <input ref={inputRef} className ='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add a new task' />
+        <button onClick={add} className='border-none rounded-full bg-orange-500 w-32 h-14 text-white text-lg font-medium cursor-pointer'> ADD + </button>
+      </div>
+
+      {/* todo list */}
+
+      <div>
+        {todoList.map((item, index) => {
+          return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
+        })}
+
+        {/* <Todoitems text="Learn Coding" />
+        <Todoitems text="Learn Coding keep learning" /> */}
+      </div>
+
+      
+
+    </div>
+  )
+}
+
+export default Todo
